6c4c1e4ce9655d304951aae47f3eeba8039495b6
GMaassen <88102642+GMaassen@users.noreply.github.com>
2023-10-19 18:12:47 +0200

    LibDB integration

--- RaapWhistle.lua
+++ RaapWhistle.lua
@@ -1,62 +1,25 @@
--- Function to toggle ground clutter
-function MyAddon_ToggleGroundClutter()
-    print("MyAddon_ToggleGroundClutter")
+function ToggleGroundClutter()
     local currentSetting = tonumber(GetCVar("graphicsGroundClutter"))
-    print(currentSetting)
-    if currentSetting  < 9 then
+    if currentSetting < 9 then
         SetCVar("graphicsGroundClutter", 9)
     else
         SetCVar("graphicsGroundClutter", 0)
     end
 end
 
-
--- Create minimap button
- 
-local minibtn = CreateFrame("Button", nil, Minimap)
-minibtn:SetFrameLevel(8)
-minibtn:SetSize(32,32)
-minibtn:SetMovable(true)
- 
--- minibtn:SetNormalTexture("Interface/AddOns/AutoSell/Leatrix_Plus_Up.blp")
--- minibtn:SetPushedTexture("Interface/AddOns/AutoSell/Leatrix_Plus_Up.blp")
--- minibtn:SetHighlightTexture("Interface/AddOns/AutoSell/Leatrix_Plus_Up.blp")
- 
-minibtn:SetNormalTexture("Interface/COMMON/Indicator-Yellow.png")
-minibtn:SetPushedTexture("Interface/COMMON/Indicator-Yellow.png")
-minibtn:SetHighlightTexture("Interface/COMMON/Indicator-Yellow.png")
- 
- 
-local myIconPos = 0
- 
--- Control movement
-local function UpdateMapBtn()
-    local Xpoa, Ypoa = GetCursorPosition()
-    local Xmin, Ymin = Minimap:GetLeft(), Minimap:GetBottom()
-    Xpoa = Xmin - Xpoa / Minimap:GetEffectiveScale() + 70
-    Ypoa = Ypoa / Minimap:GetEffectiveScale() - Ymin - 70
-    myIconPos = math.deg(math.atan2(Ypoa, Xpoa))
-    minibtn:ClearAllPoints()
-    minibtn:SetPoint("TOPLEFT", Minimap, "TOPLEFT", 52 - (80 * cos(myIconPos)), (80 * sin(myIconPos)) - 52)
-end
- 
-minibtn:RegisterForDrag("LeftButton")
-minibtn:SetScript("OnDragStart", function()
-    minibtn:StartMoving()
-    minibtn:SetScript("OnUpdate", UpdateMapBtn)
-end)
- 
-minibtn:SetScript("OnDragStop", function()
-    minibtn:StopMovingOrSizing();
-    minibtn:SetScript("OnUpdate", nil)
-    UpdateMapBtn();
-end)
- 
--- Set position
-minibtn:ClearAllPoints();
-minibtn:SetPoint("TOPLEFT", Minimap, "TOPLEFT", 52 - (80 * cos(myIconPos)),(80 * sin(myIconPos)) - 52)
- 
--- Control clicks
-minibtn:SetScript("OnClick", function()
-    MyAddon_ToggleGroundClutter()
-end)
+local miniButton = LibStub("LibDataBroker-1.1"):NewDataObject("RaapWhistle", {
+    type = "data source",
+    text = "RaapWhistle",
+    icon = "Interface\\ICONS\\Ability_hunter_beastcall",
+    OnClick = function(self, btn)
+        ToggleGroundClutter()
+    end,
+    OnTooltipShow = function(tooltip)
+        if not tooltip or not tooltip.AddLine then
+            return
+        end
+        tooltip:AddLine("RaapWhistle")
+    end
+})
+local icon = LibStub("LibDBIcon-1.0", true)
+icon:Register("RaapWhistle", miniButton, RaapWhistleDB)
